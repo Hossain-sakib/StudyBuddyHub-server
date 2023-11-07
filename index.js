00e3a3b038d4d04b8347364e17b739dcd88ce3d9
Hossain-sakib <mskib567@gmail.com>
2023-11-08 01:52:16 +0600

    created get for individual assignment id

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xpzeqy0.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,18 +33,22 @@ async function run() {
       res.send(result);
     });
 
+
+    app.get("/assignments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.findOne();
+      res.send(result);
+    });
+
+
+
     app.post("/assignments", async (req, res) => {
       const newAssignment = req.body;
       console.log(newAssignment);
       const result = await assignmentCollection.insertOne(newAssignment);
       res.send(result);
     });
-
-
-
-
-
-
 
     await client.connect();
     // Send a ping to confirm a successful connection
