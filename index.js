dab80b546b1f53599b1f7469ec7a4a451683c82e
Hossain-sakib <mskib567@gmail.com>
2023-11-08 23:40:54 +0600

    added middleware logger and verify token

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+
 // middlewares
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+// own  middlewares 
+const logger = (req, res, next) => {
+    console.log('log: info', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xpzeqy0.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -31,27 +59,27 @@ async function run() {
       .collection("myAssignments");
 
     // all assignments
-
+// get all assignment
     app.get("/assignments", async (req, res) => {
       const cursor = assignmentCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-
+// get specific assignment
     app.get("/assignments/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await assignmentCollection.findOne(query);
       res.send(result);
     });
-
+// create assignment
     app.post("/assignments", async (req, res) => {
       const newAssignment = req.body;
       console.log(newAssignment);
       const result = await assignmentCollection.insertOne(newAssignment);
       res.send(result);
     });
-
+// update assignment
     app.put("/assignments/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -74,27 +102,53 @@ async function run() {
       );
       res.send(result);
     });
+// delete specific assinment
+    app.delete("/assignments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+
 
     // my assignment
 
-    app.post("/myassignment", async (req, res) => {
-      const item = req.body;
-      console.log(item);
-      const result = await myAssignmentCollection.insertOne(item);
-      res.send(result);
-    });
+    // app.post("/myassignment", async (req, res) => {
+    //   const item = req.body;
+    //   console.log(item);
+    //   const result = await myAssignmentCollection.insertOne(item);
+    //   res.send(result);
+    // });
 
-    app.get("/myassignment", async (req, res) => {
-      const result = await myAssignmentCollection.find().toArray();
-      res.send(result);
-    });
+    // app.get("/myassignment", async (req, res) => {
+    //   const result = await myAssignmentCollection.find().toArray();
+    //   res.send(result);
+    // });
 
-    app.delete("/myassignment/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: id };
-      const result = await myAssignmentCollection.deleteOne(query);
-      res.send(result);
-    });
+    // app.delete("/myassignment/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await myAssignmentCollection.deleteOne(query);
+    //   res.send(result);
+    // });
+
+
+
+
+    // auth
+    app.post('/jwt',logger,async(req,res)=>{
+        const user = req.body;
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'});
+
+        res.cookie('token',token,{
+            httpOnly: true,
+            secure:true,
+            sameSite:'none'
+        })
+        .send({success:true});
+    })
 
     await client.connect();
     // Send a ping to confirm a successful connection
